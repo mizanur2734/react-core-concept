7c1055893c7ff213906caed704ef8e4751fcf40d
mizanur2734 <mizanurbb2791@gmail.com>
2026-02-28 22:23:29 +0600

    added the react component and function

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+
+import './App.css'
+
+function App() {
+
+function handleClick(){
+  alert('Button clicked')
+}
+
+const handleAdd5 = (num) =>{
+  const newNum = num + 5
+  alert(newNum)
+}
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <button onClick={handleClick}>Click Me</button>
+
+      <button onClick={function handleClick2(){
+        alert('Button 2 clicked')
+      }}>Click Me2</button>
+
+      <button onClick={()=>alert('Button 3 clicked')}>Click Me</button>
+      <button onClick={()=>handleAdd5(10)}>Click Add5</button>
+    </>
+  )
+}
+
+export default App
